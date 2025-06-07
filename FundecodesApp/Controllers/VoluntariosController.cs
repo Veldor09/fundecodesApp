3da0c6353d9e0a53e4d87c2b50061b2165a5b1c6
DeerK <derek.rodriguez.mendoza@est.una.ac.cr>
2025-06-06 23:24:24 -0600

    VOLUNTARIOS-AGAIN

---
+++ FundecodesApp/Controllers/VoluntariosController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using FundecodesApp.Entities;
+using FundecodesApp.Services;
+
+namespace FundecodesApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VoluntariosController : ControllerBase
+    {
+        private readonly VoluntarioService _service;
+
+        public VoluntariosController()
+        {
+            _service = new VoluntarioService();
+        }
+
+        [HttpGet]
+        public IActionResult GetVoluntarios()
+        {
+            var voluntarios = _service.GetVoluntarios();
+            return Ok(voluntarios);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetVoluntario(string id)
+        {
+            var voluntario = _service.GetVoluntarioById(id);
+            if (voluntario == null) return NotFound();
+            return Ok(voluntario);
+        }
+
+        [HttpPost]
+        public IActionResult AddVoluntario([FromBody] Voluntario voluntario)
+        {
+            _service.AddVoluntario(voluntario);
+            return CreatedAtAction(nameof(GetVoluntario), new { id = voluntario.Id }, voluntario);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateVoluntario(string id, [FromBody] Voluntario voluntario)
+        {
+            if (id != voluntario.Id) return BadRequest();
+            _service.UpdateVoluntario(voluntario);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult HideVoluntario(string id)
+        {
+            _service.HideVoluntario(id);
+            return NoContent();
+        }
+    }
