7a2cb5b7dd880713f3170935c77107293056ab01
AshVargas <vargasashly260@gmail.com>
2025-06-07 00:15:45 -0600

    Login completo

---
+++ FundecodesApp/DTO/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FundecodesApp.DTO
+{
+    public class LoginDto
+    {
+        public string Id { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Role { get; set; }
+    }
+}
