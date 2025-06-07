90044590ee45136bf7ba954d0e7415cfa1aee6d1
Sam2999 <albertsam29@gmail.com>
2025-06-06 23:28:52 -0600

    Aliados Controller

---
+++ FundecodesApp/Controllers/AliadosController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FundecodesApp.Entities;
+using FundecodesApp.Services;
+
+namespace FundecodesApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AliadosController : ControllerBase
+    {
+        private readonly AliadoService _service;
+
+        public AliadosController()
+        {
+            _service = new AliadoService();
+        }
+
+        [HttpGet]
+        public IActionResult GetAliados()
+        {
+            var aliados = _service.GetAliados();
+            return Ok(aliados);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAliado(string id)
+        {
+            var aliado = _service.GetAliadoById(id);
+            if (aliado == null) return NotFound();
+            return Ok(aliado);
+        }
+
+        [HttpPost]
+        public IActionResult AddAliado([FromBody] Aliado aliado)
+        {
+            _service.AddAliado(aliado);
+            return CreatedAtAction(nameof(GetAliado), new { id = aliado.Id }, aliado);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateAliado(string id, [FromBody] Aliado aliado)
+        {
+            if (id != aliado.Id) return BadRequest();
+            _service.UpdateAliado(aliado);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult HideAliado(string id)
+        {
+            _service.HideAliado(id);
+            return NoContent();
+        }
+
+    }
+}
+
